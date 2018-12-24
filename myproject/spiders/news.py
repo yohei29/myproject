e3587cc711b35bae6fb90ffd953e4e9b4ab865a9
yohei <yohan.k.95@gmail.com>
2018-12-25 08:18:28 +0900

    'yahooにゅーすスクレイプ'

--- myproject/spiders/news.py
+++ myproject/spiders/news.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 import scrapy
-
+from myproject.items import Headline
 
 class NewsSpider(scrapy.Spider):
     name = 'news'
@@ -8,4 +7,13 @@ class NewsSpider(scrapy.Spider):
     start_urls = ['http://news.yahoo.co.jp/']
 
     def parse(self, response):
-        pass
+        print(response.css('ul.toptopics_list a::attr("href")').extract())
+        #
+        for url in response.css('ul.toptopics_list a::attr("href")').re(r'/pickup/\d+$'):
+            yield scrapy.Request(response.urljoin(url), self.parse_topics)
+
+    def parse_topics(self, response):
+        item = Headline()
+        item['title'] = response.css('.newsTitle ::text').extract_first()
+        item['body'] = response.css('.hbody').xpath('string()').extract_first()
+        yield item
